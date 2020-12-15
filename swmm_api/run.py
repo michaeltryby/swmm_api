28f79cf7c07b0aa71b0c5eb996754166ee642ab9
Markus Pichler <markus.pichler@tugraz.at>
2020-12-15 17:28:05 +0100

    update

--- swmm_api/run.py
+++ swmm_api/run.py
@@ -8,33 +8,43 @@ __license__ = "MIT"
 import subprocess
 from os import path
 from sys import platform as _platform
-from warnings import warn
-
-
-class NoReportFileError(UserWarning):
-    pass
 
 
 class SWMMRunError(UserWarning):
     pass
 
 
-def swmm5_run(inp, rpt_dir=None, out_dir=None, init_print=False, create_out=True, swmm_path=None):
-    """
-    run a simulation with an EPA-SWMM input-file
-    default working directory is input-file directory
+def get_swmm_command_line(swmm_path, inp, rpt, out):
+    cmd = str()
+    for i in [swmm_path, inp, rpt, out]:
+        if ' ' in i:
+            cmd += f'"{i}" '
+        else:
+            cmd += f'{i} '
+    return cmd, inp, rpt, out
 
-    Args:
-        inp (str): path to input file
-        rpt_dir (str): directory in which the report-file is written.
-        out_dir (str): directory in which the output-file is written.
-        init_print (bool): if the default commandline output should be printed
-        create_out (bool): if the out-file should be created
 
-    Returns:
-        tuple[str, str, str]: INP-, RPT- and OUT-filename
-    """
-    # -----------------------
+def infer_swmm_path():
+    # UNIX
+    swmm_path = 'swmm5'
+
+    # WINDOWS
+    if _platform.startswith("win"):
+        swmm_path = None
+        # script_path = '???/swmm5.exe'
+        for program_files in ['Program Files (x86)', 'Program Files']:
+            for version in ['5.1.015', '5.1.014', '5.1.013']:
+                script_path = path.join('C:\\', program_files, 'EPA SWMM {}'.format(version), 'swmm5.exe')
+                if path.isfile(script_path):
+                    swmm_path = script_path
+                    break
+            if swmm_path is not None:
+                break
+
+    return swmm_path
+
+
+def get_swmm_command_line_auto(inp, rpt_dir=None, out_dir=None, create_out=True, swmm_path=None):
     base_filename = path.basename(inp).replace('.inp', '')
     inp_dir = path.dirname(inp)
 
@@ -53,59 +63,85 @@ def swmm5_run(inp, rpt_dir=None, out_dir=None, init_print=False, create_out=True
     else:
         out = ''
 
-    if swmm_path is None:
-        # -----------------------
-        # UNIX
-        cl_script = 'swmm5'
-
-        # WINDOWS
-        if _platform.startswith("win"):
-            cl_script = None
-            # script_path = '???/swmm5.exe'
-            for program_files in ['Program Files (x86)', 'Program Files']:
-                for version in ['5.1.015', '5.1.014', '5.1.013']:
-                    script_path = path.join('C:\\', program_files, 'EPA SWMM {}'.format(version), 'swmm5.exe')
-                    if path.isfile(script_path):
-                        cl_script = '"{}"'.format(script_path)
-                        break
-                if cl_script is not None:
-                    break
-    else:
-        cl_script = swmm_path
-
-    cmd = '{} "{}" "{}" "{}"'.format(cl_script, inp, rpt, out)
-
     # -----------------------
-    if init_print:
-        sep = '_' * 100
-        print(sep)
-        print(cmd)
-        subprocess.run(cmd, shell=True)
-        print(sep)
+    if swmm_path is None:
+        swmm_path = infer_swmm_path()
+
+    return get_swmm_command_line(swmm_path, inp, rpt, out)
+
+
+def run_swmm_stdout(command_line, sep='_'*100):
+    print(sep)
+    print(command_line)
+    subprocess.run(command_line)
+    print(sep)
+
+
+def run_swmm_custom(command_line):
+    # shell_output = subprocess.run(command_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    shell_output = subprocess.run(command_line, capture_output=True)
+    return shell_output
+
+
+def check_swmm_errors(rpt, shell_output):
+    msgs = dict()
+
+    if isinstance(shell_output, str):
+        msgs['CALL'] = shell_output
+
     else:
-        shell_output = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout = shell_output.stdout.decode()
+        # if 'error' in stdout:
+        msgs.update({
+            'CALL': shell_output.args,
+            'RETURN': shell_output.returncode,
+            'ERROR': shell_output.stderr.decode(),
+            'OUT': stdout,
+        })
 
-        error_log = shell_output.stdout.decode("utf-8")
-        if 'error' in error_log:
-            msgs = {
-                'CALL': shell_output.args,
-                'RETURN': shell_output.returncode,
-                'OUT': error_log
-            }
-            if path.isfile(rpt):
-                with open(rpt, 'r') as f:
-                    rpt_content = f.read()
-                msgs['REPORT'] = rpt_content
-            else:
-                msgs['REPORT'] = 'NO Report file created!!!'
+    if path.isfile(rpt):
+        with open(rpt, 'r') as f:
+            rpt_content = f.read()
+        if 'ERROR' in rpt_content:
+            msgs['REPORT'] = rpt_content
+    else:
+        msgs['REPORT'] = 'NO Report file created!!!'
 
-            sep = '\n' + '_' * 100 + '\n'
-            error_msg = sep + sep.join('{}:\n  {}'.format(k, v) for k, v in msgs.items())
-            raise SWMMRunError(error_msg)
+    if 'REPORT' in msgs:
+        sep = '\n' + '_' * 100 + '\n'
+        error_msg = sep + sep.join('{}:\n  {}'.format(k, v) for k, v in msgs.items())
+        raise SWMMRunError(error_msg)
+
+
+def swmm5_run(inp, rpt_dir=None, out_dir=None, init_print=False, create_out=True, swmm_path=None):
+    """
+    run a simulation with an EPA-SWMM input-file
+    default working directory is input-file directory
+
+    Args:
+        inp (str): path to input file
+        rpt_dir (str): directory in which the report-file is written.
+        out_dir (str): directory in which the output-file is written.
+        init_print (bool): if the default commandline output should be printed
+        create_out (bool): if the out-file should be created
+
+    Returns:
+        tuple[str, str, str]: INP-, RPT- and OUT-filename
+    """
+    command_line, inp, rpt, out = get_swmm_command_line_auto(inp, rpt_dir=rpt_dir, out_dir=out_dir,
+                                                             create_out=create_out, swmm_path=swmm_path)
+    # -------------------------
+    if init_print:
+        run_swmm_stdout(command_line)
+        stdout = command_line
+    else:
+        stdout = run_swmm_custom(command_line)
 
     # -------------------------
-    # check if report file is created
-    if not path.isfile(rpt):
-        raise NoReportFileError('"{}"" was not created'.format(rpt))
+    check_swmm_errors(rpt, stdout)
 
     return inp, rpt, out
+
+
+if __name__ == '__main__':
+    pass
