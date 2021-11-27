07f531ebf48649aecf0e0703b3da32569e68e552
Markus <markus.pichler@tugraz.at>
2021-11-27 20:29:25 +0100

    - updated documentations

--- swmm_api/input_file/sections/map_geodata.py
+++ swmm_api/input_file/sections/map_geodata.py
@@ -10,14 +10,14 @@ class InpSectionGeo(InpSection):
     """child class of :obj:`swmm_api.input_file.helpers.InpSection`. See parent class for all functions."""
     def __init__(self, section_object, crs="EPSG:32633"):
         """
-        create an object for ``.inp``-file sections with objects (i.e. nodes, links, subcatchments, raingages, ...)
+        create a section for ``.inp``-file with geo-objects (i.e. nodes, links, subcatchments, raingages, ...)
 
         Args:
             section_object (BaseSectionObject-like): object class which is stored in this section.
                 This information is used to set the index of the section and
                 to decide if the section can be exported (converted to a string) as a table.
             crs: Coordinate Reference System of the geometry objects.
-                Can be anything accepted by :func:`pyproj.CRS.from_user_input()`,
+                Can be anything accepted by :meth:`pyproj.CRS.from_user_input() <pyproj.crs.CRS.from_user_input>`,
                 such as an authority string (eg “EPSG:32633”) or a WKT string.
         """
         InpSection.__init__(self, section_object)
@@ -32,7 +32,7 @@ class InpSectionGeo(InpSection):
 
         Args:
             crs: Coordinate Reference System of the geometry objects.
-                Can be anything accepted by :func:`pyproj.CRS.from_user_input()`,
+                Can be anything accepted by :meth:`pyproj.CRS.from_user_input() <pyproj.crs.CRS.from_user_input>`,
                 such as an authority string (eg “EPSG:32633”) or a WKT string.
         """
         self._crs = crs
@@ -40,8 +40,8 @@ class InpSectionGeo(InpSection):
     @property
     def geo_series(self) -> GeoSeries:
         """
-        get a geopandas.GeoSeries representation for the geo-section
-        this function sets the object default crs.
+        Get a geopandas.GeoSeries representation for the geo-section.
+        This function sets the object default crs.
 
         Returns:
             GeoSeries: geo-series of the section-data
@@ -54,7 +54,7 @@ class InpSectionGeo(InpSection):
 
         Args:
             crs: Coordinate Reference System of the geometry objects.
-                Can be anything accepted by :func:`pyproj.CRS.from_user_input()`,
+                Can be anything accepted by :meth:`pyproj.CRS.from_user_input() <pyproj.crs.CRS.from_user_input>`,
                 such as an authority string (eg “EPSG:32633”) or a WKT string.
 
         Returns:
@@ -93,14 +93,30 @@ class CoordinateGeo(Coordinate):
 
 
 class VerticesGeo(Vertices):
+    """child class of :obj:`.link_component.Vertices`. See parent class for all functions."""
     _section_class = InpSectionGeo
 
     @property
     def geo(self):
+        """
+        get the shapely representation for the object (LineString).
+
+        Returns:
+            shapely.geometry.LineString: LineString object for the vertices.
+        """
         return sh.LineString(self.vertices)
 
     @classmethod
     def create_section_from_geoseries(cls, data: GeoSeries) -> InpSectionGeo:
+        """
+        create a VERTICES inp-file section for a geopandas.GeoSeries
+
+        Args:
+            data (GeoSeries): geopandas.GeoSeries of coordinates
+
+        Returns:
+            InpSectionGeo: VERTICES inp-file section
+        """
         # geometry mit MultiLineString deswegen v[0] mit ersten und einzigen linestring zu verwenden
         s = cls.create_section()
         # s.update({i: Vertices(i, v) for i, v in zip(data.index, map(lambda i: list(i.coords), data.values))})
@@ -109,14 +125,33 @@ class VerticesGeo(Vertices):
 
 
 class PolygonGeo(Polygon):
+    """child class of :obj:`.subcatch.Polygon`. See parent class for all functions."""
     _section_class = InpSectionGeo
 
     @property
     def geo(self):
+        """
+        get the shapely representation for the object (Polygon).
+
+        Returns:
+            shapely.geometry.Polygon: LineString object for the polygon.
+        """
         return sh.Polygon(self.polygon)
 
     @classmethod
     def create_section_from_geoseries(cls, data: GeoSeries) -> InpSectionGeo:
+        """
+        create a POLYGONS inp-file section for a geopandas.GeoSeries
+
+        Warnings:
+            Only uses the exterior coordinates and ignoring all interiors.
+
+        Args:
+            data (GeoSeries): geopandas.GeoSeries of polygons
+
+        Returns:
+            InpSectionGeo: POLYGONS inp-file section
+        """
         s = cls.create_section()
         s.add_multiple(s._section_object(i, [xy[0:2] for xy in list(p.coords)]) for i, p in data.exterior.iteritems())
         return s
@@ -124,14 +159,29 @@ class PolygonGeo(Polygon):
 
 ########################################################################################################################
 def convert_section_to_geosection(section: InpSection, crs="EPSG:32633") -> InpSectionGeo:
+    """
+    converting the section into a geo-section
+
+    Args:
+        section (InpSection): section (only `VERTICES`, `COORDINATES`, `POLYGONS` sections)
+        crs: Coordinate Reference System of the geometry objects.
+                Can be anything accepted by :meth:`pyproj.CRS.from_user_input() <pyproj.crs.CRS.from_user_input>`,
+                such as an authority string (eg “EPSG:32633”) or a WKT string.
+
+    Returns:
+        InpSectionGeo: geo-section
+    """
     di = {Coordinate: CoordinateGeo,
           Vertices: VerticesGeo,
           Polygon: PolygonGeo}
-    old_type = section._section_object
+    # base object of the section
+    old_type = section._section_object  # type: swmm_api.input_file.helpers.BaseSectionObject
     new_type = di[old_type]
+    # if already a geo-section
     if old_type == new_type:
         section.set_crs(crs)
         return section
+    # create new section and set crs.
     new = new_type.create_section()  # type: InpSectionGeo
     new._data = {k: new_type(**vars(section[k])) for k in section}
     new.set_crs(crs)
@@ -143,6 +193,18 @@ geo_section_converter = {COORDINATES: CoordinateGeo, VERTICES: VerticesGeo, POLY
 
 
 def add_geo_support(inp, crs="EPSG:32633"):
+    """
+    add geo-support to the inp-data
+
+    Warnings:
+        only for `VERTICES`, `COORDINATES`, `POLYGONS` sections
+
+    Args:
+        inp:
+        crs: Coordinate Reference System of the geometry objects.
+                Can be anything accepted by :meth:`pyproj.CRS.from_user_input() <pyproj.crs.CRS.from_user_input>`,
+                such as an authority string (eg “EPSG:32633”) or a WKT string.
+    """
     for sec in [VERTICES, COORDINATES, POLYGONS]:
         if (sec in inp) and not isinstance(inp[sec], InpSectionGeo):
             inp[sec] = convert_section_to_geosection(inp[sec], crs=crs)
@@ -150,17 +212,32 @@ def add_geo_support(inp, crs="EPSG:32633"):
 
 ########################################################################################################################
 def section_to_geopandas(section, crs="EPSG:32633"):
+    """
+    convert a section into a GeoSeries.
+
+    Warnings:
+        only for `VERTICES`, `COORDINATES`, `POLYGONS` sections
+
+    Args:
+        section (InpSection | InpSectionGeo): section (only `VERTICES`, `COORDINATES`, `POLYGONS`)
+        crs: Coordinate Reference System of the geometry objects.
+                Can be anything accepted by :meth:`pyproj.CRS.from_user_input() <pyproj.crs.CRS.from_user_input>`,
+                such as an authority string (eg “EPSG:32633”) or a WKT string.
+
+    Returns:
+        geopandas.GeoSeries: geo-data
+    """
     return convert_section_to_geosection(section).get_geo_series(crs)
 
 
 ########################################################################################################################
 def remove_coordinates_from_vertices(inp):
+    # SNIPPET ?!?
     new_vertices_section = dict()
     for link in inp[VERTICES]:  # type: str
         conduit = inp[CONDUITS][link]  # type: Conduit
         new_vertices = list()
-        n1 = inp[COORDINATES][conduit.FromNode]
-
+        # n1 = inp[COORDINATES][conduit.FromNode]
         new_vertices.append(inp[COORDINATES][conduit.FromNode])
         new_vertices += inp[VERTICES][link].vertices
         new_vertices.append(inp[COORDINATES][conduit.ToNode])
